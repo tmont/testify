10627f693c9f439ab472dddfcc843bfae79200a6
tmont <tommy.mont@gmail.com>
2009-06-22 06:26:47 +0000

    comments

--- src/TUnit/framework/BaseTestRunner.php
+++ src/TUnit/framework/BaseTestRunner.php
@@ -1,14 +1,68 @@
 <?php
 
+	/**
+	 * TestRunner
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * Base test runner
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	abstract class TestRunner implements RecursivelyCountable {
 		
+		/**
+		 * The tests to run
+		 *
+		 * @var array
+		 */
 		protected $tests;
+		
+		/**
+		 * The subscribing listeners
+		 *
+		 * @var array
+		 */
 		protected $listeners;
+		
+		/**
+		 * @var array
+		 */
 		protected $options;
 		
+		/**
+		 * Time the test runner began
+		 *
+		 * @var float
+		 */
 		private $startTime;
+		
+		/**
+		 * Time the test runner ended
+		 *
+		 * @var float
+		 */
 		private $endTime;
 		
+		/**
+		 * Constructor
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  array $tests
+		 * @param  array $listeners
+		 * @param  array $options
+		 */
 		public function __construct(array $tests = array(), array $listeners = array(), array $options = array()) {
 			$this->tests     = $tests;
 			$this->listeners = $listeners;
@@ -18,45 +72,135 @@
 			$this->endTime   = -1;
 		}
 		
+		/**
+		 * Gets the start time
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return float
+		 */
 		public final function getStartTime() {
 			return $this->startTime;
 		}
 		
+		/**
+		 * Gets the end time
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return float
+		 */
 		public final function getEndTime() {
 			return $this->endTime;
 		}
 		
+		/**
+		 * Gets the tests
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return array
+		 */
 		public final function getTests() {
 			return $this->tests;
 		}
 		
+		/**
+		 * Sends a warning out to all listeners
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    TestListener::onFrameworkWarning()
+		 * 
+		 * @param  string $message
+		 */
 		public final function warn($message) {
 			foreach ($this->listeners as $listener) {
 				$listener->onFrameworkWarning($message);
 			}
 		}
 		
+		/**
+		 * Sends an error message out to all listeners
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    TestListener::onFrameworkError()
+		 * 
+		 * @param  string $message
+		 */
 		public final function error($message) {
 			foreach ($this->listeners as $listener) {
 				$listener->onFrameworkError($message);
 			}
 		}
 		
+		/**
+		 * Adds a test
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  Testable $test
+		 * @return TestRunner
+		 */
 		public final function addTest(Testable $test) {
 			$this->tests[] = $test;
 			return $this;
 		}
 		
+		/**
+		 * Adds a listener
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  TestListener $listener
+		 * @return TestRunner
+		 */
 		public final function addListener(TestListener $listener) {
 			$this->listeners[] = $listener;
 			return $this;
 		}
 		
+		/**
+		 * Sets the options
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    parseOptions()
+		 * 
+		 * @param  array $options
+		 * @return TestRunner
+		 */
 		public final function setOptions(array $options) {
 			$this->options = $this->parseOptions($options);
 			return $this;
 		}
 		
+		/**
+		 * Parses options
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    getAllowableOptions()
+		 * 
+		 * @param  array $unparsed The unparsed options
+		 * @throws {@link InvalidOptionException}
+		 * @return array The parsed options
+		 */
 		protected final function parseOptions(array $unparsed) {
 			$allowedOptions = $this->getAllowableOptions();
 			$options = array_fill_keys(array_key($allowedOptions), null);
@@ -74,6 +218,18 @@
 			return $options;
 		}
 		
+		/**
+		 * Runs the tests and returns the results
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    warn()
+		 * @uses    error()
+		 * @uses    Testable::run()
+		 * 
+		 * @return array Array of {@link TestResult}s
+		 */
 		public function runTests() {
 			$results = array();
 			foreach ($this->tests as $test) {
@@ -91,12 +247,32 @@
 			return $results;
 		}
 		
+		/**
+		 * Publishes test results
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    TestListener::publishTestResults()
+		 * 
+		 * @param  array $testResults Array of {@link TestResult}s
+		 */
 		public function publishResults(array $testResults) {
 			foreach ($this->listeners as $listener) {
 				$listener->publishTestResults($testResults);
 			}
 		}
 		
+		/**
+		 * Runs all tests and publishes the results
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    TestListener::beforeTestRunner()
+		 * @uses    publishResults()
+		 * @uses    TestListener::afterTestRunner()
+		 */
 		public final function run() {
 			foreach ($this->listeners as $listener) {
 				$listener->beforeTestRunner($this);
@@ -111,14 +287,43 @@
 			}
 		}
 		
+		/**
+		 * Gets the number of tests
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return int
+		 * @ignore
+		 */
 		public function count() {
 			return count($this->tests);
 		}
 		
+		/**
+		 * Gets a detailed count of all tests
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    Util::countTests()
+		 * 
+		 * @return array The result of Util::countTests()
+		 */
 		public function getTestCount() {
 			return Util::countTests($this->tests);
 		}
 		
+		/**
+		 * Gets all allowable options for this test runner
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 *
+		 * @return array
+		 */
 		protected abstract function getAllowableOptions();
 		
 	}
--- src/TUnit/framework/TestRunners.php
+++ src/TUnit/framework/TestRunners.php
@@ -1,7 +1,37 @@
 <?php
 
+	/**
+	 * Test runners
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * Test runner for the console
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class ConsoleTestRunner extends TestRunner {
 		
+		/**
+		 * Gets allowable options
+		 *
+		 * Currently supported options:
+		 * - recursive (boolean)
+		 * - bootstrap (string)
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return array
+		 */
 		protected function getAllowableOptions() {
 			return array(
 				'recursive' => 'boolean',
