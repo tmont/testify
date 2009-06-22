e8f4f8cd7eab6bea1db25b64dc13e26dfeb31848
tmont <tommy.mont@gmail.com>
2009-06-22 09:03:08 +0000

    comments

--- src/TUnit/framework/constraints/Constraint.php
+++ src/TUnit/framework/constraints/Constraint.php
@@ -1,19 +1,75 @@
 <?php
 
+	/**
+	 * Constraint
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * Base constraint
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	*/
 	abstract class Constraint {
 		
+		/**
+		 * Fails the constraint
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  string $message
+		 * @throws {@link FailedTest}
+		 */
 		public function fail($message = '') {
 			throw new FailedTest($this->toString($message));
 		}
 		
+		/**
+		 * Gets a string representation of the constraint
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    getFailureMessage()
+		 * 
+		 * @param  string $message
+		 * @return string
+		 */
 		public function toString($message) {
 			$message = !empty($message) ? $message . "\n" : '';
 			$message .= 'Failed asserting that ' . $this->getFailureMessage();
 			return $message;
 		}
 		
+		/**
+		 * Evaluates the constraint
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 *
+		 * @return bool
+		 */
 		public abstract function evaluate();
 		
+		/**
+		 * Gets the failure message
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 *
+		 * @return string
+		 */
 		protected abstract function getFailureMessage();
 		
 	}
--- src/TUnit/framework/constraints/SimpleConstraints.php
+++ src/TUnit/framework/constraints/SimpleConstraints.php
@@ -1,69 +1,230 @@
 <?php
 
+	/**
+	 * Constraints that only take one value
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * Base class for simple constraints that only require one value
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	*/
 	abstract class SimpleConstraint extends Constraint {
 		
+		/**
+		 * The value to evaluate
+		 *
+		 * @var mixed
+		 */
 		protected $value;
 		
+		/**
+		 * Constructor
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  mixed $value
+		 */
 		public function __construct($value) {
 			$this->value = $value;
 		}
 		
 	}
 
+	/**
+	 * Constraint for asserting that a value is set
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class IssetConstraint extends SimpleConstraint {
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return bool
+		 */
 		public function evaluate() {
 			return isset($this->value);
 		}
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return string
+		 */
 		protected function getFailureMessage() {
 			return Util::export($this->value) . ' is set';
 		}
 		
 	}
 	
+	/**
+	 * Constraint for asserting that a value is empty
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class EmptyConstraint extends SimpleConstraint {
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return bool
+		 */
 		public function evaluate() {
 			return empty($this->value);
 		}
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return string
+		 */
 		protected function getFailureMessage() {
 			return Util::export($this->value) . ' is empty';
 		}
 		
 	}
 	
+	/**
+	 * Constraint for asserting that a value is null
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class NullConstraint extends SimpleConstraint {
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return bool
+		 */
 		public function evaluate() {
 			return $this->value === null;
 		}
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return string
+		 */
 		protected function getFailureMessage() {
 			return Util::export($this->value) . ' is null';
 		}
 		
 	}
 	
+	/**
+	 * Constraint for asserting that a value is true
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class TrueConstraint extends SimpleConstraint {
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return bool
+		 */
 		public function evaluate() {
 			return $this->value === true;
 		}
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return string
+		 */
 		protected function getFailureMessage() {
 			return Util::export($this->value) . ' is true';
 		}
 		
 	}
 	
+	/**
+	 * Constraint for asserting that a value is true
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class FalseConstraint extends SimpleConstraint {
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return bool
+		 */
 		public function evaluate() {
 			return $this->value === false;
 		}
 		
+		/**
+		 * {@inheritdoc}
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return string
+		 */
 		protected function getFailureMessage() {
 			return Util::export($this->value) . ' is false';
 		}
