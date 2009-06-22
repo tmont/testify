01a02e892ffc3bf227349a7739ec85050420d3d7
tmont <tommy.mont@gmail.com>
2009-06-22 06:28:12 +0000

    comments

--- src/TUnit/framework/exceptions/Exceptions.php
+++ src/TUnit/framework/exceptions/Exceptions.php
@@ -1,9 +1,45 @@
 <?php
 
+	/**
+	 * Contains all custom exception classes
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * Base exception class for TUnit
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class TUnitException extends Exception {}
 
+	/**
+	 * Exception for invalid test runner options
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 * @see     TestRunner::parseOptions()
+	 */
 	class InvalidOptionException extends TUnitException {
 		
+		/**
+		 * Constructor
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  string $switch  The invalid switch
+		 * @param  string $message
+		 */
 		public function __construct($switch, $message = '') {
 			if (!empty($message)) {
 				$message = ': ' . $message;
