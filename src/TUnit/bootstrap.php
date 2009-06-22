56a2360092ca20eed0493d7fdba7d572272c7c5b
tmont <tommy.mont@gmail.com>
2009-06-22 06:14:22 +0000

    comments

--- src/TUnit/bootstrap.php
+++ src/TUnit/bootstrap.php
@@ -1,5 +1,17 @@
 <?php
 
+	/**
+	 * Bootstrapper for TUnit
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * @see Autoloader
+	 */
 	require_once 'util/Autoloader.php';
 	
 	Autoloader::loadClassMapFromFile(dirname(__FILE__) . '/manifest.php');
