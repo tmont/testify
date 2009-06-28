d55412faa31f8932728d6018c33b81a0c0b2f0ac
tmont <tommy.mont@gmail.com>
2009-06-28 00:24:32 +0000

    comments

--- src/TUnit/framework/test/TestMethod.php
+++ src/TUnit/framework/test/TestMethod.php
@@ -1,21 +1,92 @@
 <?php
 
+	/**
+	 * TestMethod
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * Represents a single test method
+	 *
+	 * This class should only be used internally by the TUnit framework.
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	/* internal */ class TestMethod implements Testable {
 		
+		/**
+		 * @var bool
+		 */
 		protected $autoVerify;
+		
+		/**
+		 * @var lambda
+		 */
 		protected $closure;
+		
+		/**
+		 * Name of this test
+		 *
+		 * @var mixed
+		 */
 		protected $name;
 		
+		/**
+		 * Constructor
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  lambda $closure    A closure around a testable method
+		 * @param  string $name       Name of the test
+		 * @param  bool   $autoVerify
+		 */
 		public function __construct($closure, $name, $autoVerify) {
 			$this->closure    = $closure;
 			$this->autoVerify = (bool)$autoVerify;
 			$this->name       = $name;
 		}
 		
-		public function getName() {
+		/**
+		 * Gets the name of this test
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return string
+		 */
+		public final function getName() {
 			return $this->name;
 		}
 		
+		/**
+		 * Runs the test
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    TestListener::beforeTestMethod()
+		 * @uses    MockRegistry::getTrackers()
+		 * @uses    InvocationTracker::verify()
+		 * @uses    TestListener::onTestMethodPassed()
+		 * @uses    handlTestFailure()
+		 * @uses    TestListener::onTestMethodErred()
+		 * @uses    createTestResult()
+		 * @uses    TestListener::afterTestMethod()
+		 * @uses    MockRegistry::reset()
+		 * 
+		 * @param  array $listeners
+		 * @return TestResult
+		 */
 		public function run(array $listeners) {
 			foreach ($listeners as $listener) {
 				$listener->beforeTestMethod($this);
@@ -62,6 +133,17 @@
 			return $result;
 		}
 		
+		/**
+		 * Creates a test result
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  Exception $failure
+		 * @throws InvalidArgumentException
+		 * @return TestResult
+		 */
 		protected function createTestResult(Exception $failure = null) {
 			if ($failure === null) {
 				return new PassedTestResult($this);
@@ -76,6 +158,19 @@
 			throw new InvalidArgumentException('Unknown test failure type: ' . get_class($failure));
 		}
 		
+		/**
+		 * Handles a test failure
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    TestListener::onTestMethodFailed()
+		 * @uses    TestListener::onTestMethodIgnored()
+		 * @uses    TestListener::onTestMethodErred()
+		 * 
+		 * @param  TestFailure $failure
+		 * @param  array       $listeners
+		 */
 		protected function handleTestFailure(TestFailure $failure, array $listeners) {
 			if ($failure instanceof FailedTest) {
 				foreach ($listeners as $listener) {
@@ -92,10 +187,29 @@
 			}
 		}
 		
+		/**
+		 * Gets the number of tests
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return int Always returns one
+		 */
 		public function count() {
 			return 1;
 		}
 		
+		/**
+		 * Gets the number of suites, cases and methods
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    Util::countTests()
+		 * 
+		 * @return array
+		 */
 		public function getTestCount() {
 			return Util::countTests(array($this));
 		}
--- src/TUnit/framework/test/TestSuite.php
+++ src/TUnit/framework/test/TestSuite.php
@@ -1,36 +1,133 @@
 <?php
 
+	/**
+	 * TestSuite
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
+
+	/**
+	 * Represents a collection of test suites, test cases and/or
+	 * test methods
+	 *
+	 * @package TUnit
+	 * @author  Tommy Montgomery
+	 * @version 1.0
+	 * @since   1.0
+	 */
 	class TestSuite implements Testable {
 		
+		/**
+		 * The name of this test
+		 *
+		 * @var string
+		 */
 		protected $name;
+		
+		/**
+		 * The tests contained in with this suite
+		 *
+		 * @var array
+		 */
 		protected $tests;
 		
+		/**
+		 * Constructor
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  mixed $name  Name of the test suite
+		 * @param  array $tests Tests contained in this test suite
+		 */
 		public function __construct($name, array $tests = array()) {
 			$this->name  = $name;
 			$this->tests = $tests;
 		}
 		
-		public function getName() {
+		/**
+		 * Gets the name of this test
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return string
+		 */
+		public final function getName() {
 			return $this->name;
 		}
 		
+		/**
+		 * Called before the test suite runs
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 */
 		protected function setUp() {
 		
 		}
 		
+		/**
+		 * Called after the test suite runs
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 */
 		protected function tearDown() {
 		
 		}
 		
+		/**
+		 * Adds a test
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @param  Testable $test
+		 * @return TestSuite
+		 */
 		public final function addTest(Testable $test) {
 			$this->tests[] = $test;
 			return $this;
 		}
 		
+		/**
+		 * Gets all tests
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return array
+		 */
 		public final function getTests() {
 			return $this->tests;
 		}
 		
+		/**
+		 * Runs the test suite
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    TestListener::beforeTestSuite()
+		 * @uses    setUp()
+		 * @uses    CombinedTestResult::addTestResult()
+		 * @uses    TestListener::onFrameworkWarning()
+		 * @uses    tearDown()
+		 * @uses    TestListener::afterTestSuite()
+		 * 
+		 * @param  array $listeners Array of {@link TestListener}s
+		 * @return CombinedTestResult
+		 */
 		public function run(array $listeners) {
 			foreach ($listeners as $listener) {
 				$listener->beforeTestSuite($this);
@@ -57,10 +154,29 @@
 			return $result;
 		}
 		
+		/**
+		 * Gets the number of tests
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * 
+		 * @return int
+		 */
 		public function count() {
 			return count($this->tests);
 		}
 		
+		/**
+		 * Gets the number of test suites, cases and methods
+		 *
+		 * @author  Tommy Montgomery
+		 * @version 1.0
+		 * @since   1.0
+		 * @uses    Util::countTests()
+		 * 
+		 * @return array
+		 */
 		public function getTestCount() {
 			return Util::countTests($this->tests);
 		}
